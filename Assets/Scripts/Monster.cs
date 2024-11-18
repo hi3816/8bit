cabac4ba267c3b421ea7060ee72977592c1101d3
GammaTT <52464999+GammaTT@users.noreply.github.com>
2024-11-18 19:33:11 +0900

    Add Monster

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public interface IMonster
+{
+    void OnCollisionWithPlayer(Collider other)
+    {
+
+    }
+}
+
+public class Monster : MonoBehaviour
+{
+    public Vector2 pointA;
+    public Vector2 pointB;
+
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointA = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+
+        GameObject player = collision.gameObject;
+
+        if (player.transform.position.y > transform.position.y)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+
+    }
+}
