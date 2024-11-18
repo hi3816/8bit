249acdcdf25c069efe30bf5c11288bbe0316bc85
GammaTT <52464999+GammaTT@users.noreply.github.com>
2024-11-18 19:35:09 +0900

    Update Monster

--- Assets/Scripts/PatrolMonster.cs
+++ Assets/Scripts/PatrolMonster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeftRightPatrolMonster : Monster
+public class PatrolMonster : Monster
 {
     public float partolDelay;
 
