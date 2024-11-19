c8be83684ea50542779c20a60cce276d4696e19a
hi3816 <wlgp2534@naver.com>
2024-11-19 21:01:16 +0900

    Feat : UPD UIPrefabs

---
+++ Assets/Scripts/UI/LiveUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiveUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro liveCountTxt;
+
+    private void UpdateLiveCount()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
